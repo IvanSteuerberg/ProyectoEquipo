d3b6ee2a602e1d4a5ad25f4bae4b95f422ecc432
Ivan Steuerberg <irodriguezsteuerberg@danielcastelao.org>
2019-10-23 13:19:21 +0200

    Primer commit

---
+++ src/proyectoequipo/ProyectoEquipo.java
@@ -0,0 +1,10 @@
+package proyectoequipo;
+//@author irodriguezsteuerberg
+
+public class ProyectoEquipo {
+
+    public static void main(String[] args) {
+System.out.println("Hola equipo");
+    }
+    
+}
