a6af0c1975ca03d04eff3c64bd0022e309574ee9
sverdecadilla <sverdecadilla@nautilus20.ad.danielcastelao.org>
2019-10-23 14:08:30 +0200

    Cambios clase

---
+++ src/proyectoequipo/Clase1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoequipo;
+
+/**
+ *
+ * @author sverdecadilla
+ */
+public class Clase1 {
+private int numero;
+public void setNumero(int numero){
+this.numero=numero;    
+}
+public int getNumero(){
+return numero;    
+}
+}
+
+//Si, he copiado tu clase
